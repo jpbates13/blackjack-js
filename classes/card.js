b8678337382388797b89f1134f9bfcba5401ac94
jpbates13 <jpbates13@gmail.com>
2019-08-27 10:26:41 -0400

    Defined cards and stored images

---
+++ classes/card.js
@@ -0,0 +1,22 @@
+function Card(cardRank, cardSuit, cardPointValue, cardFaceDown){
+	this.faceDown = cardFaceDown;
+	this.rank = cardRank;
+	this.suit = cardSuit;
+	this.pointValue = cardPointValue;
+	this.image = '<img class = "card" src="./assets/cards/';
+	if(this.faceDown){
+		console.log("here");
+		this.image = this.image + 'red_back.png" />';
+	}else if(this.pointValue == 10 && cardRank != "ten"){
+		this.image = this.image + this.rank.substring(0,1) + this.suit.substring(0,1) + '.png" />';
+	} else {
+		this.image = this.image + String(this.pointValue) + this.suit.substring(0,1) + '.png" />';
+	}
+	this.text = function(){
+		return this.rank + " of " + this.suit + " (point value = " + String(this.pointValue) + ")";
+	}
+
+	console.log("Image Source: " + this.image);
+	console.log(this.rank + " of " + this.suit);
+
+}
